e72c96bda1a58211bf0589643e16d647bf1e5587
Vitja72 <viktor.stefan@tn.techstarter.de>
2025-08-19 11:56:55 +0200

    feat: Added blockquote and footer elements.

--- got-quote-generator/src/App.js
+++ got-quote-generator/src/App.js
@@ -1,13 +1,21 @@
 import './App.css';
 
+const quote = "Der Winter naht.";
+const character = "Ned Stark"; 
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
        <h1>Game of Thrones Zitat-Generator</h1>
        <p>Ein Ort für Weisheit (und Sarkasmus) aus Westeros.</p>
-
-      </header>
+       </header>
+       <main>
+        <blockquote>{quote}</blockquote>
+       </main>
+       <footer>
+        - {character}
+       </footer>
     </div>
   );
 }
